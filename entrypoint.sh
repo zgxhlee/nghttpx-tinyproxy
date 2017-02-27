2dbc5842e63d2390976943b8e10206b2b8f54c75
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-02-27 20:36:32 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 /tinyproxy/tinyproxy
+
 cd /nghttp2
+if [ "$CERT_PEM" != "$KEY_PEM" ]; then
+  echo -e "$CERT_PEM" > server.crt
+  echo -e "$KEY_PEM"  > server.key
+fi
+
 ./nghttpx -s -f'*,8443' -b127.0.0.1,8888 server.key server.crt
