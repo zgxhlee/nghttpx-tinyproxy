9d6051ae8f228a5112cef3fd5decbc6c8ef6523a
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-02-28 14:41:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
-service tinyproxy start
+traffic_ctl config set proxy.config.url_remap.remap_required 0
+traffic_ctl config set proxy.config.http.cache.http 1
+traffic_ctl config set proxy.config.reverse_proxy.enabled 0
+trafficserver start
 
 cd /nghttp2
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
@@ -6,4 +9,4 @@ if [ "$CERT_PEM" != "$KEY_PEM" ]; then
   echo -e "$KEY_PEM"  > server.key
 fi
 
-./nghttpx -s -f'*,8443' -b127.0.0.1,8888 server.key server.crt
+nghttpx -s -f'*,8443' -b127.0.0.1,8080 /nghttp2/server.key /nghttp2/server.crt
