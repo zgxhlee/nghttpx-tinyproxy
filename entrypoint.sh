a85ff93912f10d36675b94f427a30bcefc09c0e1
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-02-28 18:29:27 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ if [ "$CERT_PEM" != "$KEY_PEM" ]; then
   echo -e "$SERVER_KEY" > /etc/nghttpx/server.key
 fi
 
-nghttpx -s -f'*,8443' -b127.0.0.1,8080 /etc/nghttpx/server.key /etc/nghttpx/server.crt
+nghttpx -s -f'*,8443' -b127.0.0.1,8888 /etc/nghttpx/server.key /etc/nghttpx/server.crt
