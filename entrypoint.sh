1e41dc7aabab50738e319b27c30902886190347a
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-02-28 16:29:22 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
+traffic_ctl server start
 traffic_ctl config set proxy.config.url_remap.remap_required 0
 traffic_ctl config set proxy.config.http.cache.http 1
 traffic_ctl config set proxy.config.reverse_proxy.enabled 0
 traffic_ctl config reload
-traffic_ctl start
+traffic_ctl server restart
 
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
-  echo -e "$CERT_PEM" > /etc/server.crt
-  echo -e "$KEY_PEM"  > /etc/server.key
+  echo -e "$SERVER_CRT" > /etc/server.crt
+  echo -e "$SERVER_KEY" > /etc/server.key
 fi
 
 nghttpx -s -f'*,8443' -b127.0.0.1,8080 /etc/server.key /etc/server.crt
