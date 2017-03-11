c1d1b19bcbb305d189f220dcbac4e4faa46170af
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-03-11 18:00:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 traffic_server -R 1
 
 if [ "$SERVER_CRT" != "$SERVER_KEY" ]; then
-  echo -e "$SERVER_CRT" > /etc/nghttpx/server.crt
-  echo -e "$SERVER_KEY" > /etc/nghttpx/server.key
+  echo -e "$SERVER_CRT" > /var/server.crt
+  echo -e "$SERVER_KEY" > /var/server.key
 fi
 
-nghttpx -s -f'*,8443' -b127.0.0.1,8080 /etc/nghttpx/server.key /etc/nghttpx/server.crt
+nghttpx -s -f'*,8443' -b127.0.0.1,8080 /var/server.key /var/server.crt
