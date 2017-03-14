b8e745654ba6a85ad6cff6f5f938ff2c87468be1
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-03-14 22:22:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@ echo -e "$SERVER_CRT" > /config/server.crt
 echo -e "$SERVER_KEY" > /config/server.key
 
 tinyproxy -c /config/tinyproxy.conf
-nghttpx -s -f'*,8443' -b127.0.0.1,8080 /config/server.key /config/server.crt
+nghttpx -s -k -f'*,8443' -b127.0.0.1,8080 /config/server.key /config/server.crt
