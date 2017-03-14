3c963a46463b36e2d216217ba202ed590c1e8665
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-03-14 21:38:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
-traffic_server -R 1
+echo -e "$SERVER_CRT" > /config/server.crt
+echo -e "$SERVER_KEY" > /config/server.key
 
-if [ "$SERVER_CRT" != "$SERVER_KEY" ]; then
-  echo -e "$SERVER_CRT" > /var/server.crt
-  echo -e "$SERVER_KEY" > /var/server.key
-fi
-
-nghttpx -s -f'*,8443' -b127.0.0.1,8080 /var/server.key /var/server.crt
+tinyproxy -c /config/tinyproxy.conf
+nghttpx -s -f'*,8443' -b127.0.0.1,8080 /config/server.key /config/server.crt
