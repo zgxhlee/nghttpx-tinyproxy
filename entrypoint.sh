03f5d06e4eebc5385781e793bce089df131bc0af
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-03-15 12:28:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 echo -e "$SERVER_CRT" > /config/server.crt
 echo -e "$SERVER_KEY" > /config/server.key
 tinyproxy -c /config/tinyproxy.conf
-nghttpx -conf /config/nghttpx.conf
+nghttpx --conf /config/nghttpx.conf
+# nghttpx -s -k -f'*,8443' -b127.0.0.1,8080 -4 --tls-min-proto-version=TLSv1.2 --tls-max-proto-version=TLSv1.3 --no-ocsp server.key server.crt
